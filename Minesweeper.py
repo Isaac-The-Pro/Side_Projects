edea87df768fb795227ed8cd28347df0854fffd5
Isaac Proctor <204100068+Isaac-The-Pro@users.noreply.github.com>
2026-03-09 10:48:12 -0700

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -16,6 +16,7 @@ class Game:
         self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
         root.resizable(False, False)
         self.tiles = []
+        self.isRunning = True
         for y in range(self.minesX):
            rowTiles = []
            for x in range(self.minesY):
@@ -44,7 +45,20 @@ class Game:
                 xy = self.canvas.coords(tile.object)
                 if (xy[0] <= event.x <= xy[2]) and (xy[1] <= event.y <= xy[3]):
                     tile.click(game.canvas)
-
+                    return
+    def rightClick(self, event):
+        for row in self.tiles:
+            for tile in row:
+                xy = self.canvas.coords(tile.object)
+                if (xy[0] <= event.x <= xy[2]) and (xy[1] <= event.y <= xy[3]):
+                    tile.addFlag(game.canvas)
+    def lose(self):
+        root.destroy()
+        print("game over")
+    def win(self):
+        root.destroy()
+        print("you won!")
+        self.isRunning = False
                
 class Tile:
     def __init__(self, x, y, width, canvas):
@@ -56,17 +70,20 @@ class Tile:
        self.hasMine = False
        self.touchesMines = 0
        self.clicked=False
+       self.flag=None
+       self.flag = canvas.create_oval(self.realX+width/4, self.realY+width/4, self.realX+3*width/4, self.realY+3*width/4, fill="red", state='hidden')
+       self.hasFlag = False
     def click(self, canvas):
-        print("tile was clicked: ", self.x, self.y)
         canvas.itemconfig(self.object, state='hidden')
         self.clicked=True
-        if self.hasMine == True:
-            pass
-        else:
-            self.getSurroundingTiles(self.listX, self.listY, self.tiles)
-            for tile in self.surroundingTiles:
-                if self.numOfMines==0 and tile.clicked==False:
-                    tile.click(canvas)
+        self.getSurroundingTiles(self.listX, self.listY, self.tiles)
+        for tile in self.surroundingTiles:
+            if self.numOfMines==0 and tile.clicked==False:
+                tile.click(canvas)
+        if self.flag:
+            canvas.itemconfig(self.flag, state='hidden')
+        if self.hasMine == True and self.hasFlag == False:
+            game.lose()
     def getSurroundingTiles(self, listX, listY, tiles):
         self.surroundingTiles = []
         try:
@@ -108,7 +125,15 @@ class Tile:
         self.listX = listX
         self.listY = listY
         self.tiles = tiles
-
+    def addFlag(self, canvas):
+        print("addflag called")
+        if self.hasFlag == False:
+                canvas.itemconfig(self.flag, state='normal')
+                self.hasFlag = True
+        else:
+                canvas.itemconfig(self.flag, state='hidden')
+                self.hasFlag = False
+            
 class Mine:
     def __init__(self, x, y, width, canvas):
         self.x = x
@@ -118,10 +143,21 @@ class Mine:
         self.object = canvas.create_rectangle(self.realX, self.realY, self.realX + width, self.realY + width, fill="red", outline="black")
 
 def click_handler(event):
-    if event.num == 1:
-        game.leftClick(event)
-
-game = Game(20, 20, 20)
+    try:
+        if event.num == 1:
+            game.leftClick(event)
+            winGame = True
+            for row in game.tiles:
+                for tile in row:
+                    if tile.hasMine == False and tile.clicked == False:
+                        winGame = False
+            if winGame:
+                game.win()
+        elif event.num == 3:
+            game.rightClick(event)
+    except:
+        pass
+game = Game(20, 20, 40)
 
 root.bind("<Button>", click_handler)
 root.mainloop()
